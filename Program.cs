4966b5cf95093179046d96c72dc6f90477baaae4
Leonard Roussard <51063205+Lionvsx@users.noreply.github.com>
2021-11-23 16:37:04 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TD_7_A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
